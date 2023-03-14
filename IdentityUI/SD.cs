73fc4647c4af3542ed0677ac2193165198ae5555
Simon <simon.gurung@thimphutechpark.bt>
2023-03-14 14:34:08 +0600

    Git ignore modified

--- IdentityUI/SD.cs
+++ IdentityUI/SD.cs
@@ -35,8 +35,8 @@ public static class SD
                 ClientId="mango",
                 ClientSecrets={new Secret("secret".Sha256())},
                 AllowedGrantTypes=GrantTypes.Code,
-                RedirectUris={"http://localhost:5200/signin-oidc"},
-                PostLogoutRedirectUris={"http://localhost:5200/signout-callback-oidc"},
+                RedirectUris={"http://localhost:5033/signin-oidc"},
+                PostLogoutRedirectUris={"http://localhost:5033/signout-callback-oidc"},
                 AllowedScopes= new List<string>
                 {
                     IdentityServerConstants.StandardScopes.OpenId,
